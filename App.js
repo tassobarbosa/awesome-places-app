25aa20da225b181df456bcd3e777f7cd40c22e03
tassoeb <tasso_barbosa@yahoo.com.br>
2018-08-06 09:42:24 -0300

    Components refactored

--- App.js
+++ App.js
@@ -1,53 +1,27 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import ListItem from './src/components/ListItem/ListItem';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
 export default class App extends Component {
   state = {
-    placeName: '',
     places: []
-  }
-  placeNameChangedHandler = val =>{
-    this.setState({
-      placeName: val
+  };
+
+  placeAddedHandler = placeName => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(placeName)
+      };
     });
   };
 
-placeSubmitHandler = () => {
-  if(this.state.placeName.trim() === ""){
-    return;
-  }
-
-  this.setState(prevState =>{
-    return {
-      places: prevState.places.concat(prevState.placeName)
-    };
-  });
-};
-
   render() {
-    const placesOutput = this.state.places.map((place,i) => (
-      <ListItem key={i} placeName={place}/>
-    ));
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.placeInput}
-            placeholder="An awesome place"
-            value={this.state.placeName}
-            onChangeText={this.placeNameChangedHandler}
-          />
-          <Button
-            style={styles.placeButton}
-            title='Add'
-            onPress={this.placeSubmitHandler}
-          />
-        </View>
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -56,24 +30,9 @@ placeSubmitHandler = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  inputContainer:{
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  placeInput:{
-    width: '70%',
-  },
-  placeButton:{
-    width: '30%',
-  },
-  listContainer:{
-    width: '100%'
-  },
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const listItem = (props) => (
-  <View style={styles.listItem}>
-    <Text>{props.placeName}</Text>
-  </View>
+    <View style={styles.listItem}>
+        <Text>{props.placeName}</Text>
+    </View>
 );
 
 const styles = StyleSheet.create({
-  listItem: {
-    width: "100%",
-    marginBottom: 5,
-    padding: 10,
-    backgroundColor: '#eee'
-  }
+    listItem: {
+        width: "100%",
+        marginBottom: 5,
+        padding: 10,
+        backgroundColor: "#eee"
+    }
 });
 
 export default listItem;
