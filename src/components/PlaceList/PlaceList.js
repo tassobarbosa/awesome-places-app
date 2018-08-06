0177eb820f92f11cc290733eee8373df95d902e2
tassoeb <tasso_barbosa@yahoo.com.br>
2018-08-06 14:14:12 -0300

    FlatList

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem
-          key={i}
-          placeName={place}
-          onItemPressed={() => props.onItemDeleted(i) }/>
-      ));
     return (
-        <View style={styles.listContainer}>{placesOutput}</View>
+        <FlatList
+          style={styles.listContainer}
+          data={props.places}
+          renderItem = {(info) =>(
+            <ListItem
+                  placeName={info.item.value}
+                  onItemPressed={() => props.onItemDeleted(info.item.key) }
+                />
+          )}
+        />
     );
 };
 
