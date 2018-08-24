c95084619acba5e296561e99cdfaffcb130551e4
tassoeb <tasso_barbosa@yahoo.com.br>
2018-08-24 09:25:25 -0300

    input place and list place working

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,13 +1,10 @@
 import {
   ADD_PLACE,
   DELETE_PLACE,
-  SELECT_PLACE,
-  DESELECT_PLACE
 } from "../actions/actionTypes";
 
 const initialState = {
-  places: [],
-  selectedPlace: null
+  places: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -30,20 +27,8 @@ const reducer = (state = initialState, action) => {
         places: state.places.filter(place => {
           return place.key !== state.selectedPlace.key;
         }),
-        selectedPlace: null
       };
-    case SELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: state.places.find(place => {
-          return place.key === action.placeKey;
-        })
-      };
-    case DESELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: null
-      };
+
     default:
       return state;
   }
