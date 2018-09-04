22b23c486e62ceea71d00d8eb30574b11dab614b
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-04 17:01:53 -0300

    DefaultInput implemented - Lesson 7-89

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -10,12 +11,33 @@ class AuthScreen extends Component {
 
   render () {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler}/>
+        <Button title="Switch Log In" onPress={this.loginHandler}/>
+        <View style={styles.containerInput}>
+          <DefaultInput placeholder='Your e-mail address' style={styles.input}/>
+          <DefaultInput placeholder='Password' style={styles.input}/>
+          <DefaultInput placeholder='Password confirmation' style={styles.input}/>
+        </View>
+        <Button title="Submit" onPress={this.loginHandler}/>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerInput: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
+  }
+});
 
 export default AuthScreen;
