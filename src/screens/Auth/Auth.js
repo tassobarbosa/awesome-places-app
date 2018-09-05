b27f1dcda444a60de03799c8b4943898ea1a046b
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-05 10:50:01 -0300

    Reusable UI components created 7-93

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text, Button, TextInput, StyleSheet} from 'react-native';
+import {View, Text, Button, TextInput, StyleSheet, ImageBackground} from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
+import BackgroundImage from '../../assets/background.jpg';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -11,21 +15,29 @@ class AuthScreen extends Component {
 
   render () {
     return (
-      <View style={styles.container}>
-        <Text>Auth Screen</Text>
-        <Button title="Switch Log In" onPress={this.loginHandler}/>
-        <View style={styles.containerInput}>
-          <DefaultInput placeholder='Your e-mail address' style={styles.input}/>
-          <DefaultInput placeholder='Password' style={styles.input}/>
-          <DefaultInput placeholder='Password confirmation' style={styles.input}/>
+      <ImageBackground source={BackgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Log In</HeadingText>
+          </MainText>
+          <ButtonWithBackground color="#29aaf4" onPress={() => alert('Hello')}>Switch to Log In</ButtonWithBackground>
+          <View style={styles.containerInput}>
+            <DefaultInput placeholder='Your e-mail address' style={styles.input}/>
+            <DefaultInput placeholder='Password' style={styles.input}/>
+            <DefaultInput placeholder='Password confirmation' style={styles.input}/>
+          </View>
+          <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Submit</ButtonWithBackground>
         </View>
-        <Button title="Submit" onPress={this.loginHandler}/>
-      </View>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  backgroundImage:{
+    width: '100%',
+    flex: 1
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
