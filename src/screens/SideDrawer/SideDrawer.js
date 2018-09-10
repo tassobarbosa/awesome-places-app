f8113a33ea8f52815741cb98c20bdbc50b3c76e2
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-10 11:51:19 -0300

    signout button on drawer

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class SideDrawer extends Component {
   render() {
@@ -10,7 +12,12 @@ class SideDrawer extends Component {
           { width: Dimensions.get("window").width * 0.8 }
         ]}
       >
-        <Text>SideDrawer</Text>
+        <TouchableOpacity>
+          <View style={styles.drawerItem}>
+            <Icon name="ios-log-out" size={30} color="#bbb" style={styles.drawerItemIcon}/>
+            <Text>Sign Out</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,9 +25,18 @@ class SideDrawer extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 22,
+    paddingTop: 50,
     backgroundColor: "white",
-    flex: 1
+    flex: 1,
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: "#eee"
+  },
+  drawerItemIcon: {
+    marginRight: 10
   }
 });
 
