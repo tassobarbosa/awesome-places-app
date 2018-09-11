da20c1d7953a2e8a966354f003a5138b6a8ebc00
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-11 11:29:29 -0300

    addind validation 8-121

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -15,44 +15,110 @@ import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 import backgroundImage from "../../assets/background.jpg";
+import validate from "../../utility/validation";
 
 class AuthScreen extends Component {
   state = {
-    viewMode: Dimensions.get('window').height > 500 ? "portrait" : "landscape"
-  }
+    viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+    controls: {
+      email: {
+        value: "",
+        valid: false,
+        validationRules: {
+          isEmail: true
+        }
+      },
+      password: {
+        value: "",
+        valid: false,
+        validationRules: {
+          minLength: 6
+        }
+      },
+      confirmPassword: {
+        value: "",
+        valid: false,
+        validationRules: {
+          equalTo: "password"
+        }
+      }
+    }
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     Dimensions.addEventListener("change", this.updateStyles);
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     Dimensions.removeEventListener("change", this.updateStyles);
   }
 
-  updateStyles = (dims) => {
+  updateStyles = dims => {
     this.setState({
-      viewMode:
-        dims.window.height > 500 ? "portrait" : "landscape"
+      viewMode: dims.window.height > 500 ? "portrait" : "landscape"
     });
-  }
+  };
 
   loginHandler = () => {
     startMainTabs();
   };
 
-  render() {
+  updateInputState = (key, value) => {
+    let connectedValue = {};
+    if (this.state.controls[key].validationRules.equalTo) {
+      const equalControl = this.state.controls[key].validationRules.equalTo;
+      const equalValue = this.state.controls[equalControl].value;
+      connectedValue = {
+        ...connectedValue,
+        equalTo: equalValue
+      };
+    }
+    if (key === "password") {
+      connectedValue = {
+        ...connectedValue,
+        equalTo: value
+      };
+    }
+    this.setState(prevState => {
+      return {
+        controls: {
+          ...prevState.controls,
+          confirmPassword: {
+            ...prevState.controls.confirmPassword,
+            valid:
+              key === "password"
+                ? validate(
+                    prevState.controls.confirmPassword.value,
+                    prevState.controls.confirmPassword.validationRules,
+                    connectedValue
+                  )
+                : prevState.controls.confirmPassword.valid
+          },
+          [key]: {
+            ...prevState.controls[key],
+            value: value,
+            valid: validate(
+              value,
+              prevState.controls[key].validationRules,
+              connectedValue
+            )
+          }
+        }
+      };
+    });
+  };
 
+  render() {
     let headingText = null;
 
-    if (this.state.viewMode === "portrait"){
-      headingText  = (
+    if (this.state.viewMode === "portrait") {
+      headingText = (
         <MainText>
           <HeadingText>Please Log In</HeadingText>
         </MainText>
       );
     }
-
     return (
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
         <View style={styles.container}>
@@ -64,13 +130,44 @@ class AuthScreen extends Component {
             <DefaultInput
               placeholder="Your E-Mail Address"
               style={styles.input}
+              value={this.state.controls.email.value}
+              onChangeText={val => this.updateInputState("email", val)}
             />
-            <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
-              <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                <DefaultInput placeholder="Password" style={styles.input} />
+            <View
+              style={
+                this.state.viewMode === "portrait"
+                  ? styles.portraitPasswordContainer
+                  : styles.landscapePasswordContainer
+              }
+            >
+              <View
+                style={
+                  this.state.viewMode === "portrait"
+                    ? styles.portraitPasswordWrapper
+                    : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput
+                  placeholder="Password"
+                  style={styles.input}
+                  value={this.state.controls.password.value}
+                  onChangeText={val => this.updateInputState("password", val)}
+                />
               </View>
-              <View style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                <DefaultInput placeholder="Confirm Password" style={styles.input} />
+              <View
+                style={
+                  this.state.viewMode === "portrait"
+                    ? styles.portraitPasswordWrapper
+                    : styles.landscapePasswordWrapper
+                }
+              >
+                <DefaultInput
+                  placeholder="Confirm Password"
+                  style={styles.input}
+                  value={this.state.controls.confirmPassword.value}
+                  onChangeText={val =>
+                    this.updateInputState("confirmPassword", val)}
+                />
               </View>
             </View>
           </View>
@@ -101,18 +198,18 @@ const styles = StyleSheet.create({
     borderColor: "#bbb"
   },
   landscapePasswordContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   portraitPasswordContainer: {
-    flexDirection: 'column',
-    justifyContent: 'flex-start'
+    flexDirection: "column",
+    justifyContent: "flex-start"
   },
   landscapePasswordWrapper: {
-    width: '45%',
+    width: "45%"
   },
   portraitPasswordWrapper: {
-    width: '100%',
+    width: "100%"
   }
 });
 
