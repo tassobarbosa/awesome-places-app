40cf46c46552ce363b80d184c98661cbc9462fbf
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-25 15:01:07 -0300

    finding current location on android 9-139

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -9,18 +9,65 @@ class PickLocation extends Component {
       longitude: -122.4013726,
       latitudeDelta: 0.0122,
       longitudeDelta: Dimensions.get('window').width/Dimensions.get('window').height * 0.0122
-    }
+    },
+    locationChosen: false
+  }
+
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    })
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        },
+        locationChosen: true
+      };
+    });
+  }
+
+  getLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const coordsEvent = {
+        nativeEvent: {
+          coordinate: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude
+          }
+        }
+      };
+      this.pickLocationHandler(coordsEvent);
+    },
+  err => {
+    console.log(err);
+    alert("Fetching the Position failed, please pick one manually");
+  })
   }
 
   render(){
+    let marker = null;
+
+    if(this.state.locationChosen){
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+    }
     return(
       <View style={styles.container}>
         <MapView
           initialRegion={this.state.focusedLocation}
+          onPress={this.pickLocationHandler}
           style={styles.map}
-        />
+          ref = {ref => this.map = ref}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
-          <Button title="Locate Me" onPress={() => alert('Pick Location')}/>
+          <Button title="Locate Me" onPress={this.getLocationHandler}/>
         </View>
       </View>
     )
