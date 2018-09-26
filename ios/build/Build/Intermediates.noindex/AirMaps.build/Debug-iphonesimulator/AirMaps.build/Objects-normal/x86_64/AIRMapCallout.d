81309594e131820f02d52df0835081e2b2baa47d
Bee Labs <beelabs@Bees-Mac-mini.local>
2018-09-26 14:05:50 -0300

    working on iphone simulator

---
+++ ios/build/Build/Intermediates.noindex/AirMaps.build/Debug-iphonesimulator/AirMaps.build/Objects-normal/x86_64/AIRMapCallout.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/beelabs/native-test/awesome-places-app/node_modules/react-native-maps/lib/ios/AirMaps/AIRMapCallout.m \
+  /Users/beelabs/native-test/awesome-places-app/node_modules/react-native-maps/lib/ios/AirMaps/AIRMapCallout.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTView.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTComponent.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h
---
+++ ios/build/Build/Intermediates.noindex/RNImagePicker.build/Debug-iphonesimulator/RNImagePicker.build/Objects-normal/x86_64/ImagePickerManager.d
@@ -0,0 +1,16 @@
+dependencies: \
+  /Users/beelabs/native-test/awesome-places-app/node_modules/react-native-image-picker/ios/ImagePickerManager.m \
+  /Users/beelabs/native-test/awesome-places-app/node_modules/react-native-image-picker/ios/ImagePickerManager.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/beelabs/native-test/awesome-places-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h
