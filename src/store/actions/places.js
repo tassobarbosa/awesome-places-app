31d03706628ef2d889b77ac7e74860e4a0125aff
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-27 14:44:16 -0300

    sending image base64 to firebase 157-10

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,19 +2,30 @@ import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-      const placeData = {
-        name: placeName,
-        location: location,
-      };
-      fetch("https://awesomeplaces-216819.firebaseio.com/places.json",{
-        method: "POST",
-        body: JSON.stringify(placeData)
-      })
-      .catch(err => console.log(err))
-      .then(res => res.json())
-      .then(parsedRes => {
-        console.log(parsedRes);
-      });
+        fetch("https://us-central1-awesomeplaces-216819.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://awesomeplaces-216819.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
     };
 };
 
