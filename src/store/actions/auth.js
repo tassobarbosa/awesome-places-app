3e43d5a860ced6cf6c63692b6a476f7e9ebb3661
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-28 10:50:49 -0300

    creating auth user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,31 @@
 import { TRY_AUTH } from './actionTypes';
 
 export const tryAuth = (authData) => {
-    return {
-        type: TRY_AUTH,
-        authData: authData
+    return dispatch => {
+      dispatch(authSignup(authData));
     };
+};
+
+export const authSignup = authData => {
+  return dispatch => {
+    fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDrP68FxkbfOKJW-l6B6CDN5F550fSqRzQ",{
+      method: "POST",
+      body: JSON.stringify({
+        email: authData.email,
+        password: authData.password,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).
+    catch(err => {
+      console.log(err);
+      alert("Authentication failed, please try again");
+    })
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
+    })
+  };
 };
