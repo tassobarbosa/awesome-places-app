98fd187100d1e350c61884a650353a01e31ee37e
tassoeb <tasso_barbosa@yahoo.com.br>
2018-09-28 11:33:56 -0300

    signin an signup methods working 11-171

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,31 +1,42 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH } from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./index";
+import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
-export const tryAuth = (authData) => {
-    return dispatch => {
-      dispatch(authSignup(authData));
-    };
-};
-
-export const authSignup = authData => {
+export const tryAuth = (authData, authMode) => {
   return dispatch => {
-    fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDrP68FxkbfOKJW-l6B6CDN5F550fSqRzQ",{
-      method: "POST",
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-        returnSecureToken: true
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).
-    catch(err => {
-      console.log(err);
-      alert("Authentication failed, please try again");
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      console.log(parsedRes);
-    })
+    dispatch(uiStartLoading());
+const apiKey = "AIzaSyDrP68FxkbfOKJW-l6B6CDN5F550fSqRzQ";
+    let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" + apiKey;
+    if (authMode === "signup") {
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" + apiKey
+    }
+    fetch(
+        url,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: authData.email,
+            password: authData.password,
+            returnSecureToken: true
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      )
+        .catch(err => {
+          console.log(err);
+          alert("Authentication failed, please try again!");
+          dispatch(uiStopLoading());
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+          dispatch(uiStopLoading());
+          if (parsedRes.error) {
+            alert("Authentication failed, please try again!");
+          } else {
+            startMainTabs();
+          }
+        });
   };
 };
